db2f0bb70bb1afec6334502a22d047d582db7f18
newwohh <nevoznvx@gmail.com>
2023-08-30 23:29:37 +0530

    updated-login

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,7 +1,68 @@
+"use client";
+
 import React from "react";
+import { titleHandler } from "@/actions/actions";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Link from "next/link";
+import { loginstyles } from "@/styles/Login";
 
 const Login = () => {
-  return <div>Login</div>;
+  React.useEffect(() => {
+    titleHandler("Login");
+  }, []);
+
+  return (
+    <div style={loginstyles.logincontainer}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box>
+          <Typography variant="h4">Login</Typography>
+          <Typography>Welcome back</Typography>
+        </Box>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: "40px",
+          }}
+        >
+          <TextField
+            label="Email"
+            placeholder="Email"
+            sx={{ width: "400px", marginBottom: "30px" }}
+            variant="standard"
+          />
+          <TextField
+            label="Password"
+            placeholder="Password"
+            type="password"
+            sx={{ width: "400px", marginBottom: "" }}
+            variant="standard"
+          />
+          <Button variant="text" sx={loginstyles.loginbtn}>
+            Create account
+          </Button>
+        </form>
+        <Link
+          style={{
+            color: "black",
+            textDecoration: "none",
+          }}
+          href={"/forgotpassword"}
+        >
+          Forgot password?
+        </Link>
+      </Box>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/styles/Login.ts
@@ -0,0 +1,22 @@
+export const loginstyles = {
+  logincontainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    width: "200vh",
+    backgroundColor: "#f6f6f6",
+    backgroundImage: "linear-gradient(315deg, #f6f6f6 0%, #e9e9e9 74%)",
+  },
+  loginbtn: {
+    width: "300px",
+    height: "50px",
+    backgroundColor: "black",
+    color: "white",
+    marginTop: "30px",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+    },
+  },
+};
